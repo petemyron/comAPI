3e9b74568b372ab8e706b791121fcf5865935f21
Pete Myron <pete.myron@gmail.com>
2011-06-06 14:43:16 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ComAPI::Application.config.secret_token = '3f164a753757734fd9ff9a7740e33c506ba9ff8939c3fac46dba26e41347bd1ea5dfed5cfd3c680a326c2fa17bd2f0afb416b11a62df9a75f06c28156fb6243d'
